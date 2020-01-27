e50389dffd1f46be2b953da19bb3534f728d818a
Arne Bartenbach <st161059@stud.uni-stuttgart.de>
2020-01-27 23:19:44 +0100

    WaypointRouteTest update

--- app/src/androidTest/java/de/uni_stuttgart/informatik/sopra/sopraapp/WaypointRouteTest.java
+++ app/src/androidTest/java/de/uni_stuttgart/informatik/sopra/sopraapp/WaypointRouteTest.java
@@ -95,6 +95,10 @@ public class WaypointRouteTest {
         onView(withId(R.id.etRouteName)).perform(typeText("Marathon"), closeSoftKeyboard());
         //add wp1
         onView(withId(R.id.btnAddWaypoint)).perform(click());
+        onData(hasToString(startsWith(""))).inAdapterView(withId(R.id.recyclerListWp)).atPosition(0).check(matches(withText("1: Eingang Pos 539.5;886.5, NoteHier geht es los, ")));
+        onData(hasToString(startsWith(""))).inAdapterView(withId(R.id.recyclerListWp)).atPosition(1).check(matches(withText("2: Schatzkammer Pos 539.5;886.5, NoteNichts mitgehen lassen!, ")));
+        onData(hasToString(startsWith(""))).inAdapterView(withId(R.id.recyclerListWp)).atPosition(2).check(matches(withText("3: WC Pos 539.5;886.5, NoteAuch das muss bewacht werden, ")));
+        onData(hasToString(startsWith(""))).inAdapterView(withId(R.id.recyclerListWp)).atPosition(3).check(matches(withText("4: Ausgang Pos 539.5;886.5, NoteJetzt aber raus hier, ")));
         onData(hasToString(startsWith("1"))).perform(click());
         onView(withId(R.id.durationInput)).perform(typeText("2"), closeSoftKeyboard());
         onView(withText("OK")).perform(click());
@@ -121,7 +125,10 @@ public class WaypointRouteTest {
         onData(hasToString(startsWith("2"))).perform(click());
         onView(withId(R.id.durationInput)).perform(typeText("99"), closeSoftKeyboard());
         onView(withText("OK")).perform(click());
-
+        onData(hasToString(startsWith(""))).inAdapterView(withId(R.id.selectedWaypointList)).atPosition(0).check(matches(withText("2: Eingang Pos 539.5;886.5, NoteHier geht es los, ")));
+        onData(hasToString(startsWith(""))).inAdapterView(withId(R.id.selectedWaypointList)).atPosition(1).check(matches(withText("2: Schatzkammer Pos 539.5;886.5, NoteNichts mitgehen lassen!, ")));
+        onData(hasToString(startsWith(""))).inAdapterView(withId(R.id.selectedWaypointList)).atPosition(3).check(matches(withText("4: Ausgang Pos 539.5;886.5, NoteJetzt aber raus hier, ")));
+        onData(hasToString(startsWith(""))).inAdapterView(withId(R.id.selectedWaypointList)).atPosition(4).check(matches(withText("4: Ausgang Pos 539.5;886.5, NoteJetzt aber raus hier, ")));
         onView(withId(R.id.saveRoute)).perform(click());
 
         // Route 2
